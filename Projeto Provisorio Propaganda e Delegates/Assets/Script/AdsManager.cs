fd594bdc1c75d97e1b621b432ad1763def068725
pssgiasmim <iasmimpassing@gmail.com>
2024-10-29 16:27:07 -0300

    Git Ignore

---
+++ Projeto Provisorio Propaganda e Delegates/Assets/Script/AdsManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour , IUnityAdsInitializationListener, IUnityAdsShowListener
+{
+    public void OnInitializationComplete()
+    {
+        
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
